f2d26c8265fba6205f5b407334775c95573f861d
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-08-16 10:42:10 +0300

    Modified About component to display the basic aim of the app

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1 className='heading'>About Us</h1>
+        <p className='body'>A fast-paced lifestyle needs a service that will take the reins in finding your perfect match, lover and life partner. XOXO finds singles to match your style and personality, in the international aspect of love.Our agency is not just about finding a partner to match with but about finding a long-lasting true partner who you can form deep connections with. Therefore, we are doing tremendous work in selecting couples and you go on a date only with the person with whom you have great chances for a joint future.Don't waste an opportunity to find the one you've been searching for. </p>
+    </div>
+  )
+}
+
+export default About
